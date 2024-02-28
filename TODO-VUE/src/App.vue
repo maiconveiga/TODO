a0f23cf1393760b63dc48ca3101d824e6bb6bb86
= <=>
2024-02-28 13:29:46 -0300

    ADD/ Separeted into components

--- TODO-VUE/src/App.vue
+++ TODO-VUE/src/App.vue
@@ -1,38 +1,48 @@
 <script setup>
 import { reactive } from 'vue';
-
+import Cabecalho from './components/Cabecalho.vue';
+import Formulario from './components/Formulario.vue';
+import ListaDeTarefas from './components/ListaDeTarefas.vue';
 
 const estado = reactive({
-  tarefaTemp: '',
+  tarefaTemp: 'aa',
   filtro: 'todas',
-  tarefas:[
+  tarefas: [
     {
-      titulo: '',
+      titulo: 'teste',
       finalizada: false,
     },
-    ] 
+    {
+      titulo: 'teste',
+      finalizada: false,
+    },
+    {
+      titulo: 'teste',
+      finalizada: false,
+    },
+  ]
 });
 
-const getTarefasPendentes = () =>{
+const getTarefasPendentes = () => {
   return estado.tarefas.filter(tarefa => tarefa.finalizada === false)
 }
-const getTarefasFinalizadas = () =>{
+const getTarefasFinalizadas = () => {
   return estado.tarefas.filter(tarefa => tarefa.finalizada === true)
 }
 
-const getTarefasFiltradas = () =>{
-  const {filtro} = estado;
-  switch(filtro){
+const getTarefasFiltradas = () => {
+  const { filtro } = estado;
+  switch (filtro) {
     case 'pendentes':
       return getTarefasPendentes();
     case 'finalizadas':
       return getTarefasFinalizadas();
     default:
       return estado.tarefas;
-  } 
+  }
 }
 
-const cadastraTarefa = () =>{
+const cadastraTarefa = () => {
   const tarefaNova = {
     titulo: estado.tarefaTemp,
     finalizada: false,
@@ -44,46 +54,8 @@ const cadastraTarefa = () =>{
 
 <template>
   <div class="container">
-    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
-      <h1>Minhas tarefas</h1>
-      <p>
-        Você possui {{ getTarefasPendentes().length }} tarefas pendentes
-      </p>
-    </header>
-    <form @submit.prevent="cadastraTarefa">
-      <div class="row">
-        <div class="col">
-          <input :value="estado.tarefaTemp" @change="evento => estado.tarefaTemp = evento.target.value" required type="text" name="" id="" placeholder="Descrição da tarefa" class="form-control">
-        </div>
-        <div class="col-md-2">
-          <button class="btn btn-primary" type="submit">Cadastrar</button>
-        </div>
-        <div class="col-md-2">
-          <select @change="evento => estado.filtro = evento.target.value" class="form-control">
-            <option value="todas">Todas tarefas</option>
-            <option value="pendentes">Pendentes</option>
-            <option value="finalizadas">Finalizadas</option>
-          </select>
-        </div>
-      </div>
-    </form>
-    <ul class="list-group mt-4">
-      <li class="list-group-item" v-for="tarefa in getTarefasFiltradas()">
-        <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada" type="checkbox" :id="tarefa.titulo">
-        <label :for="tarefa.titulo" class="ms-3" :class="{done:tarefa.finalizada === true}">
-          {{tarefa.titulo}}
-        </label>
-      </li>
-    </ul>
+    <Cabecalho :tarefasPendentes="getTarefasPendentes().length"></Cabecalho>
+    <Formulario :trocarFiltro="evento => estado.filtro = evento.target.value" :tarefaTemp="estado.tarefaTemp" :editaTarefaTemp="evento => estado.tarefaTemp = evento.target.value" :cadastraTarefa="cadastraTarefa"></Formulario>
+    <ListaDeTarefas :getTarefasFiltradas="getTarefasFiltradas()"></ListaDeTarefas>
   </div>
-  <form>
-
-  </form>
 </template>
-
-<style scoped>
-
-.done{
-  text-decoration: line-through;
-}
-</style>
---
+++ TODO-VUE/src/components/ListaDeTarefas.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps(['getTarefasFiltradas']);
+</script>
+
+<template>
+    <ul class="list-group mt-4">
+        <li class="list-group-item" v-for="tarefa in props.getTarefasFiltradas">
+            <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
+                type="checkbox" :id="tarefa.titulo">
+            <label :for="tarefa.titulo" class="ms-3" :class="{ done: tarefa.finalizada === true }">
+                {{ tarefa.titulo }}
+            </label>
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+.done {
+    text-decoration: line-through;
+}
+</style>
